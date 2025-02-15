19cf3fd79d947ac0fac03aa20f9a4f5d9bbaea65
vastja <vastja@gmail.com>
2025-02-15 17:32:37 +0100

    Initial endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+async fn hello_world() -> &'static str {
+    "Hello, Rust API!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
