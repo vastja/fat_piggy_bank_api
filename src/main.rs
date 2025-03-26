396092f6e9c6fe5760f7beba478d169796c2f181
vastja <vastja@gmail.com>
2025-03-26 18:37:12 +0100

    Expenses endpoint with static data

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use chrono::{DateTime, TimeZone, Utc};
+use serde::Serialize;
 use tokio::net::TcpListener;
-
-async fn hello_world() -> &'static str {
-    "Hello, Rust API!"
-}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello_world));
+    let app = Router::new().route("/api", get(expenses));
 
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn expenses() -> Json<Vec<Expense>> {
+    let expenses = vec![Expense {
+        id: 0,
+        date: Utc.with_ymd_and_hms(2025, 5, 17, 0, 0, 0).unwrap(),
+        tag: String::from("Groccery"),
+        amount: 100,
+    }];
+    Json(expenses)
+}
+
+#[derive(Serialize)]
+struct Expense {
+    id: u32,
+    date: DateTime<Utc>,
+    tag: String,
+    amount: i32,
 }
