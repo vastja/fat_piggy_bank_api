836e5c0e02043e1abccc1cc5fbdb8d3a5040a7d6
vastja <vastja@gmail.com>
2025-03-31 22:18:46 +0200

    group expenses param

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use axum::{routing::get, Json, Router};
+use axum::{extract::Query, routing::get, Json, Router};
 use chrono::{DateTime, Utc};
 use rusqlite::{Connection, Result};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api", get(expenses));
+    let app = Router::new().route("/api/expenses", get(expenses));
 
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     axum::serve(listener, app.into_make_service())
@@ -14,13 +14,23 @@ async fn main() {
         .unwrap();
 }
 
-async fn expenses() -> Json<Vec<Expense>> {
+#[derive(Deserialize)]
+struct ExpensesParams {
+    group: bool,
+}
+
+async fn expenses(Query(params): Query<ExpensesParams>) -> Json<Vec<Expense>> {
     let connection = Connection::open("../fat_piggy_bank_importer/fat_piggy_bank.db")
         .expect("Database connection failed.");
 
-    let mut select = connection
-        .prepare("SELECT * FROM expenses")
-        .expect("Retrieving expenses failed.");
+    let mut select = match params.group {
+        true => connection
+            .prepare("SELECT id, MIN(date), tag, SUM(amount) FROM expenses GROUP BY tag")
+            .expect("Retrieving expenses failed."),
+        false => connection
+            .prepare("SELECT * FROM expenses")
+            .expect("Retrieving expenses failed."),
+    };
 
     let expenses: Result<Vec<Expense>> = select
         .query_map([], |row| {
